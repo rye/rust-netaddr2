65c9f0ded03520745151a7083f7bbea8a22baa95
Kristofer Rye <kristofer.rye@gmail.com>
2019-07-03 17:13:11 -0500

    Add a couple more benchmarks to the parsing bench
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- benches/netaddr-parse.rs
+++ benches/netaddr-parse.rs
@@ -6,8 +6,20 @@ extern crate netaddr2;
 use netaddr2::NetAddr;
 
 #[bench]
-fn bench_parse_v4(b: &mut test::Bencher) {
+fn bench_parse_v4_cidr(b: &mut test::Bencher) {
 	let net = "127.0.0.1/8";
+	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
+}
+
+#[bench]
+fn bench_parse_v4_extended(b: &mut test::Bencher) {
+	let net = "127.0.0.1 255.0.0.0";
+	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
+}
+
+#[bench]
+fn bench_parse_v4_extended_slash(b: &mut test::Bencher) {
+	let net = "127.0.0.1/255.0.0.0";
 	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
 }
 
