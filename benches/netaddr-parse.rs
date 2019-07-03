f3a55bb9c53f4222ab440d12d06f6a42b3168b31
Kristofer Rye <kristofer.rye@gmail.com>
2019-07-03 17:19:24 -0500

    Add more benchmarks for IPv6-land
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- benches/netaddr-parse.rs
+++ benches/netaddr-parse.rs
@@ -24,7 +24,37 @@ fn bench_parse_v4_extended_slash(b: &mut test::Bencher) {
 }
 
 #[bench]
-fn bench_parse_v6(b: &mut test::Bencher) {
+fn bench_parse_v6_cidr(b: &mut test::Bencher) {
 	let net = "2001:db8:dead:beef::/32";
+	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
+}
+
+#[bench]
+fn bench_parse_v6_extended(b: &mut test::Bencher) {
+	let net = "2001:db8:dead:beef::0 ffff:ffff::0";
+	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
+}
+
+#[bench]
+fn bench_parse_v6_extended_slash(b: &mut test::Bencher) {
+	let net = "2001:db8:dead:beef::0/ffff:ffff::0";
+	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
+}
+
+#[bench]
+fn bench_parse_v6_cidr_full(b: &mut test::Bencher) {
+	let net = "2001:0db8:dead:beef:0000:0000:0000:0000/32";
+	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
+}
+
+#[bench]
+fn bench_parse_v6_extended_full(b: &mut test::Bencher) {
+	let net = "2001:0db8:dead:beef:0000:0000:0000:0000 ffff:ffff:0000:0000:0000:0000:0000:0000";
+	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
+}
+
+#[bench]
+fn bench_parse_v6_extended_slash_full(b: &mut test::Bencher) {
+	let net = "2001:0db8:dead:beef:0000:0000:0000:0000/ffff:ffff:0000:0000:0000:0000:0000:0000";
 	b.iter(|| assert!(net.parse::<NetAddr>().is_ok()))
 }
