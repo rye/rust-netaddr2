94c22035d4285c447a2ff898728284831ae3afb6
Kristofer Rye <kristofer.rye@gmail.com>
2019-07-04 19:58:09 -0500

    Add a couple more derived common traits
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use core::cmp::Ordering;
 use core::str::FromStr;
 use std::net::IpAddr;
 
-#[derive(Copy, Clone, Hash, Debug)]
+#[derive(Copy, Clone, Debug, Eq, Hash, Ord)]
 pub struct NetAddr {
 	pub netmask: IpAddr,
 	pub network: IpAddr,
