6695efee5679c7b22104cf9a0a58748fec9fe130
Kristofer Rye <kristofer.rye@gmail.com>
2019-07-04 19:17:16 -0500

    Revamp cidr-parse example
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- examples/cidr-parse.rs
+++ examples/cidr-parse.rs
@@ -1,24 +1,14 @@
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::net::IpAddr;
 
 extern crate netaddr2;
 use netaddr2::NetAddr;
 
 fn main() {
-	let net: NetAddr = "127.0.0.1/8".parse().unwrap();
-	assert_eq!(net.netmask, IpAddr::V4(Ipv4Addr::new(255, 0, 0, 0)));
-	assert_eq!(net.network, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 0)));
+	let net: NetAddr = "192.0.2.0/24".parse().unwrap();
+	assert_eq!(net.network, IpAddr::V4("192.0.2.0".parse().unwrap()));
+	assert_eq!(net.netmask, IpAddr::V4("255.255.255.0".parse().unwrap()));
 
-	let net: NetAddr = "192.168.16.1/22".parse().unwrap();
-	assert_eq!(net.netmask, IpAddr::V4(Ipv4Addr::new(255, 255, 252, 0)));
-	assert_eq!(net.network, IpAddr::V4(Ipv4Addr::new(192, 168, 16, 0)));
-
-	let net: NetAddr = "ff02::1/8".parse().unwrap();
-	assert_eq!(
-		net.netmask,
-		IpAddr::V6(Ipv6Addr::new(0xff00, 0, 0, 0, 0, 0, 0, 0x0000))
-	);
-	assert_eq!(
-		net.network,
-		IpAddr::V6(Ipv6Addr::new(0xff00, 0, 0, 0, 0, 0, 0, 0x0000))
-	);
+	let net: NetAddr = "2001:db8:dead:beef::/32".parse().unwrap();
+	assert_eq!(net.network, IpAddr::V6("2001:db8::".parse().unwrap()));
+	assert_eq!(net.netmask, IpAddr::V6("ffff:ffff::".parse().unwrap()));
 }
