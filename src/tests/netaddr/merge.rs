33d942b94f59497a297266c47cd98c82608a545d
Kristofer Rye <kristofer.rye@gmail.com>
2019-07-17 15:22:53 -0500

    Add a test for v6's panic
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/tests/netaddr/merge.rs
+++ src/tests/netaddr/merge.rs
@@ -38,5 +38,16 @@ fn v4_adjacent_but_not_mergable_none() {
 	let b: NetAddr = "10.0.2.0/24".parse().unwrap();
 
 	assert_eq!(a.merge(&b), None);
+	assert_eq!(b.merge(&a), None);
 	assert_eq!(a.merge(&b), b.merge(&a));
+}
+
+#[test]
+#[should_panic]
+fn v6_not_implemented() {
+	let a: NetAddr = "2001:db8:dead:beef::/64".parse().unwrap();
+	let b: NetAddr = "2001:db8:dead:beee::/64".parse().unwrap();
+
+	assert_eq!(a.merge(&b), None);
+	assert_eq!(b.merge(&a), None);
 }
