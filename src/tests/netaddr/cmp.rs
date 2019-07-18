cc82893bdfc45834dbc665049fba149e109fee9a
Kristofer Rye <kristofer.rye@gmail.com>
2019-07-18 09:39:42 -0500

    t/n/cmp: Add some tests for v6 cmp and DRY
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/tests/netaddr/cmp.rs
+++ src/tests/netaddr/cmp.rs
@@ -1,11 +1,13 @@
 use super::*;
 
+use std::cmp::Ordering;
+
 #[test]
 fn v4_different_networks() {
 	let a: NetAddr = "1.0.0.0/8".parse().unwrap();
 	let b: NetAddr = "2.0.0.0/8".parse().unwrap();
 
-	assert_eq!(a.partial_cmp(&b), Some(std::cmp::Ordering::Less))
+	assert_eq!(a.partial_cmp(&b), Some(Ordering::Less))
 }
 
 #[test]
@@ -13,7 +15,7 @@ fn v4_different_netmasks() {
 	let a: NetAddr = "1.0.0.0/7".parse().unwrap();
 	let b: NetAddr = "1.0.0.0/8".parse().unwrap();
 
-	assert_eq!(a.partial_cmp(&b), Some(std::cmp::Ordering::Less))
+	assert_eq!(a.partial_cmp(&b), Some(Ordering::Less))
 }
 
 #[test]
@@ -21,7 +23,7 @@ fn v4_different() {
 	let a: NetAddr = "1.0.0.0/8".parse().unwrap();
 	let b: NetAddr = "0.0.0.0/24".parse().unwrap();
 
-	assert_eq!(a.partial_cmp(&b), Some(std::cmp::Ordering::Greater))
+	assert_eq!(a.partial_cmp(&b), Some(Ordering::Greater))
 }
 
 #[test]
@@ -29,5 +31,37 @@ fn v4_equal() {
 	let a: NetAddr = "1.0.0.0/8".parse().unwrap();
 	let b: NetAddr = "1.0.0.0/8".parse().unwrap();
 
-	assert_eq!(a.partial_cmp(&b), Some(std::cmp::Ordering::Equal))
+	assert_eq!(a.partial_cmp(&b), Some(Ordering::Equal))
+}
+
+#[test]
+fn v6_different_networks() {
+	let a: NetAddr = "2001:db8:0:0::0/64".parse().unwrap();
+	let b: NetAddr = "2001:db8:0:1::0/64".parse().unwrap();
+
+	assert_eq!(a.partial_cmp(&b), Some(Ordering::Less))
+}
+
+#[test]
+fn v6_different_netmasks() {
+	let a: NetAddr = "2001:db8:0:0::0/63".parse().unwrap();
+	let b: NetAddr = "2001:db8:0:0::0/64".parse().unwrap();
+
+	assert_eq!(a.partial_cmp(&b), Some(Ordering::Less))
+}
+
+#[test]
+fn v6_different() {
+	let a: NetAddr = "ff02::1/16".parse().unwrap();
+	let b: NetAddr = "2001:db8:0:1::0/64".parse().unwrap();
+
+	assert_eq!(a.partial_cmp(&b), Some(Ordering::Greater))
+}
+
+#[test]
+fn v6_equal() {
+	let a: NetAddr = "2001:db8:dead:beef::0/64".parse().unwrap();
+	let b: NetAddr = "2001:db8:dead:beef::0/64".parse().unwrap();
+
+	assert_eq!(a.partial_cmp(&b), Some(Ordering::Equal))
 }
