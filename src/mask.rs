ea2e2f41d0192d3129f4937d754718f9cb7b98c2
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-05 17:05:12 -0500

    Implement Mask for IpAddr
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/mask.rs
+++ src/mask.rs
@@ -1,3 +1,4 @@
+use std::net::IpAddr;
 use std::net::Ipv4Addr;
 use std::net::Ipv6Addr;
 
@@ -5,6 +6,18 @@ pub trait Mask {
 	type Output;
 
 	fn mask(&self, other: &Self) -> Self::Output;
+}
+
+impl Mask for IpAddr {
+	type Output = Result<Self, &'static str>;
+
+	fn mask(&self, other: &Self) -> Self::Output {
+		match (self, other) {
+			(Self::V4(a), Self::V4(b)) => Ok(Self::V4(a.mask(&b))),
+			(Self::V6(a), Self::V6(b)) => Ok(Self::V6(a.mask(&b))),
+			(_, _) => Err("mismatched address types"),
+		}
+	}
 }
 
 impl Mask for Ipv4Addr {
