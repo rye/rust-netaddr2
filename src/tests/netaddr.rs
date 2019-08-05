08c4b4d39b29f1f8229396c42463f538eef11a3f
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-05 17:46:35 -0500

    Add tests for Send-Syncness of Netv4Addr and Netv6Addr
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/tests/netaddr.rs
+++ src/tests/netaddr.rs
@@ -16,6 +16,38 @@ fn is_sync() {
 	assert_sync::<NetAddr>();
 }
 
+mod netv4addr {
+	use super::Netv4Addr;
+
+	#[test]
+	fn is_send() {
+		fn assert_send<T: Send>() {}
+		assert_send::<Netv4Addr>();
+	}
+
+	#[test]
+	fn is_sync() {
+		fn assert_sync<T: Sync>() {}
+		assert_sync::<Netv4Addr>();
+	}
+}
+
+mod netv6addr {
+	use super::Netv6Addr;
+
+	#[test]
+	fn is_send() {
+		fn assert_send<T: Send>() {}
+		assert_send::<Netv6Addr>();
+	}
+
+	#[test]
+	fn is_sync() {
+		fn assert_sync<T: Sync>() {}
+		assert_sync::<Netv6Addr>();
+	}
+}
+
 mod broadcast;
 mod cmp;
 mod contains;
