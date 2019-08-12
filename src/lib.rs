9ca008bb99851302a815892051ab5e9169a16045
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-12 13:24:57 -0500

    Move impl for PartialOrd into Netv4Addr's module
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -198,16 +198,6 @@ impl FromStr for Netv6Addr {
 	}
 }
 
-impl PartialOrd for Netv4Addr {
-	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-		match self.addr().partial_cmp(other.addr()) {
-			Some(Ordering::Equal) => self.mask().partial_cmp(other.mask()),
-			Some(ordering) => Some(ordering),
-			None => None,
-		}
-	}
-}
-
 impl PartialOrd for Netv6Addr {
 	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
 		match self.addr().partial_cmp(other.addr()) {
--- src/netv4addr.rs
+++ src/netv4addr.rs
@@ -58,3 +58,13 @@ impl Merge for Netv4Addr {
 		}
 	}
 }
+
+impl PartialOrd for Netv4Addr {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		match self.addr().partial_cmp(other.addr()) {
+			Some(Ordering::Equal) => self.mask().partial_cmp(other.mask()),
+			Some(ordering) => Some(ordering),
+			None => None,
+		}
+	}
+}
