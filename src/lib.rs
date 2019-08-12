54564fe14d4b71c3cd30105c1b94d72eaaa726d9
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-12 13:40:00 -0500

    Move impl From<Ipv4Addr> for Netv4Addr into the netv4addr mod
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
-impl From<Ipv4Addr> for Netv4Addr {
-	fn from(addr: Ipv4Addr) -> Self {
-		Self::new(addr, Ipv4Addr::from(u32::max_value()))
-	}
-}
-
 impl From<Ipv6Addr> for Netv6Addr {
 	fn from(addr: Ipv6Addr) -> Self {
 		Self::new(addr, Ipv6Addr::from(u128::max_value()))
--- src/netv4addr.rs
+++ src/netv4addr.rs
@@ -49,6 +49,12 @@ impl Contains for Netv4Addr {
 	}
 }
 
+impl From<Ipv4Addr> for Netv4Addr {
+	fn from(addr: Ipv4Addr) -> Self {
+		Self::new(addr, Ipv4Addr::from(u32::max_value()))
+	}
+}
+
 impl FromStr for Netv4Addr {
 	type Err = NetAddrError;
 
