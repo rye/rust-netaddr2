74cd73212e67d37826d73b1cc6c170f7c307acbc
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-12 13:35:59 -0500

    Move impl Mask for foreign types back into src/lib
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::net::{Ipv4Addr, Ipv6Addr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 impl From<Ipv4Addr> for NetAddr {
 	fn from(addr: Ipv4Addr) -> Self {
@@ -21,6 +21,36 @@ impl From<Ipv6Addr> for NetAddr {
 impl From<Ipv6Addr> for Netv6Addr {
 	fn from(addr: Ipv6Addr) -> Self {
 		Self::new(addr, Ipv6Addr::from(u128::max_value()))
+	}
+}
+
+impl Mask for IpAddr {
+	type Output = Result<Self, &'static str>;
+
+	fn mask(&self, other: &Self) -> Self::Output {
+		match (self, other) {
+			// TODO convert to Self::V4, Self::V6 once stabilized in 1.37 (2019-08-15)
+			(IpAddr::V4(a), IpAddr::V4(b)) => Ok(IpAddr::V4(a.mask(&b))),
+			// TODO convert to Self::V4, Self::V6 once stabilized in 1.37 (2019-08-15)
+			(IpAddr::V6(a), IpAddr::V6(b)) => Ok(IpAddr::V6(a.mask(&b))),
+			(_, _) => Err("mismatched address types"),
+		}
+	}
+}
+
+impl Mask for Ipv4Addr {
+	type Output = Self;
+
+	fn mask(&self, other: &Self) -> Self::Output {
+		Self::Output::from((u32::from(*self)) & (u32::from(*other)))
+	}
+}
+
+impl Mask for Ipv6Addr {
+	type Output = Self;
+
+	fn mask(&self, other: &Self) -> Self::Output {
+		Self::Output::from((u128::from(*self)) & (u128::from(*other)))
 	}
 }
 
