6d74e8b85ae99e74ae3e1c7f44d23984b3f2b54a
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-12 11:26:58 -0500

    Test the fmt::Display trait
    I didn't put two and two together that the different types of formatting
    can actually be tested independently by the use of the format! macro!
    
    This makes so much sense.  I like it.  This should also cover the
    yet-uncovered lines as well.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/tests/netaddr_error.rs
+++ src/tests/netaddr_error.rs
@@ -18,3 +18,14 @@ fn is_display() {
 	fn assert_display<T: Display>() {}
 	assert_display::<NetAddrError>();
 }
+
+#[cfg(test)]
+mod parse_error {
+	use super::NetAddrError;
+	#[test]
+	fn right_message() {
+		let error: NetAddrError = NetAddrError::ParseError("INNER_TEXT".into());
+		let result: &str = &format!("{}", error);
+		assert_eq!(result, "unable to parse address: INNER_TEXT");
+	}
+}
