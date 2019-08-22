ce0afe4d03e8073d97b1d998e238e6b5094a6078
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-22 17:18:29 -0500

    Add tests of Contains for Netv6Addr
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/netv6addr/contains.rs
+++ src/netv6addr/contains.rs
@@ -11,3 +11,26 @@ impl Contains for Netv6Addr {
 		other.addr().mask(&self.mask()) == *self.addr()
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use std::net::Ipv6Addr;
+
+	#[test]
+	fn ip() {
+		let net: Netv6Addr = "2001:db8:d00b::/48".parse().unwrap();
+		assert!(net.contains(&Ipv6Addr::new(0x2001, 0x0db8, 0xd00b, 0, 0, 0, 0, 0x0001)));
+		assert!(net.contains(&Ipv6Addr::new(
+			0x2001, 0x0db8, 0xd00b, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff
+		)));
+		assert!(!net.contains(&Ipv6Addr::new(0x2001, 0x0db8, 0xd00c, 0, 0, 0, 0, 1)));
+	}
+
+	#[test]
+	fn net() {
+		let net: Netv6Addr = "2001:db8:d000::/40".parse().unwrap();
+		let net_inner: Netv6Addr = "2001:db8:d00b::/48".parse().unwrap();
+		assert!(net.contains(&net_inner));
+	}
+}
