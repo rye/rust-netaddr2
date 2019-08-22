e34f7dec0a06bb9a0ab5fcb6d33dccb0434b7297
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-22 17:11:13 -0500

    Add test of From<Ipv4Addr> for Netv4Addr
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/netv4addr/from/ipv4addr.rs
+++ src/netv4addr/from/ipv4addr.rs
@@ -6,3 +6,18 @@ impl From<Ipv4Addr> for Netv4Addr {
 		Self::new(addr, Ipv4Addr::from(u32::max_value()))
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn uses_max_netmask() {
+		let addr: Ipv4Addr = "192.0.2.42".parse().unwrap();
+		let netaddr: Netv4Addr = Netv4Addr::from(addr);
+		assert_eq!(
+			netaddr,
+			Netv4Addr::new(addr, Ipv4Addr::from(u32::max_value()))
+		);
+	}
+}
