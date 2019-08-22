f550b9342cca8f53eb0287e21d4dfe2ab190b06f
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-22 17:16:34 -0500

    Add tests of Contains for Netv4Addr
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/netv4addr/contains.rs
+++ src/netv4addr/contains.rs
@@ -12,3 +12,24 @@ impl Contains for Netv4Addr {
 		other.addr().mask(&self.mask()) == *self.addr()
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use std::net::Ipv4Addr;
+
+	#[test]
+	fn ip() {
+		let net: Netv4Addr = "127.0.0.1/8".parse().unwrap();
+		assert!(net.contains(&Ipv4Addr::new(127, 0, 0, 1)));
+		assert!(net.contains(&Ipv4Addr::new(127, 127, 255, 1)));
+		assert!(!net.contains(&Ipv4Addr::new(64, 0, 0, 0)));
+	}
+
+	#[test]
+	fn net() {
+		let net: Netv4Addr = "127.0.0.1/8".parse().unwrap();
+		let net_inner: Netv4Addr = "127.128.0.1/24".parse().unwrap();
+		assert!(net.contains(&net_inner));
+	}
+}
