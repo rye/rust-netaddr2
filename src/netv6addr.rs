5b539cc5486a1427450778d0e5d2c2215caef365
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-22 19:28:48 -0500

    Document Netv6Addr::new
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/netv6addr.rs
+++ src/netv6addr.rs
@@ -21,6 +21,20 @@ impl Netv6Addr {
 		&self.addr
 	}
 
+	/// Create a new `Netv6Addr` from the given `addr` and `mask`.
+	///
+	/// Masks the given `addr` value with the given `mask` before
+	/// the structure containing both is returned.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// # use netaddr2::Netv6Addr;
+	/// # use std::net::Ipv6Addr;
+	/// let network = Ipv6Addr::new(0x2001, 0xdb8, 0, 0, 0, 0, 0, 0x42);
+	/// let netmask = Ipv6Addr::new(0xff00, 0, 0, 0, 0, 0, 0, 0);
+	/// let netaddr = Netv6Addr::new(network, netmask);
+	/// ```
 	pub fn new(addr: Ipv6Addr, mask: Ipv6Addr) -> Self {
 		let addr = addr.mask(&mask);
 		Self { addr, mask }
