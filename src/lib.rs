22fcc7f80d3b4824e4c3af75fff37d1a1bab7139
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-25 10:48:30 -0500

    Reorganize lib.rs
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 mod netaddr;
 mod netv4addr;
 mod netv6addr;
+mod netaddr_error;
+mod traits;
+
 pub use netaddr::*;
 pub use netv4addr::*;
 pub use netv6addr::*;
-
-mod netaddr_error;
 pub use netaddr_error::*;
-
-mod traits;
 pub use traits::*;
