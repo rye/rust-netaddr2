4c99e67e772a7175104cf1ee83306d3c422d88d3
Kristofer Rye <kristofer.rye@gmail.com>
2020-01-09 09:43:41 -0600

    iter/address: Set up the Net.*AddrAddressIterator structs
    (I should probably use a generic...)
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/iter/address.rs
+++ src/iter/address.rs
@@ -1,3 +1,24 @@
+use crate::NetAddr;
+use crate::Netv4Addr;
+use crate::Netv6Addr;
+
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
+pub struct NetAddrAddressIterator {
+	net: NetAddr,
+	cur: IpAddr,
+}
+
+pub struct Netv4AddrAddressIterator {
+	net: Netv4Addr,
+	cur: Ipv4Addr,
+}
+
+pub struct Netv6AddrAddressIterator {
+	net: Netv6Addr,
+	cur: Ipv6Addr,
+}
+
 #[cfg(test)]
 mod tests {
 }
