7c454557106abd5d68f3c76b016f5541f6af3f7d
Kristofer Rye <kristofer.rye@gmail.com>
2020-01-09 10:08:30 -0600

    Add Iterator behavior for NetAddrAddressIterator
    Tests coming soon.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/iter/address.rs
+++ src/iter/address.rs
@@ -2,11 +2,46 @@ use crate::NetAddr;
 use crate::Netv4Addr;
 use crate::Netv6Addr;
 
+use crate::Contains;
+
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 pub struct NetAddrAddressIterator {
 	net: NetAddr,
 	cur: Option<IpAddr>,
+}
+
+impl Iterator for NetAddrAddressIterator {
+	type Item = IpAddr;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		let cur: Option<IpAddr> = self.cur;
+		let next: Option<IpAddr> = match cur {
+			Some(IpAddr::V4(ipv4)) => u32::from(ipv4)
+				.checked_add(1)
+				.map(|next: u32| IpAddr::V4(Ipv4Addr::from(next))),
+			Some(IpAddr::V6(ipv6)) => u128::from(ipv6)
+				.checked_add(1)
+				.map(|next: u128| IpAddr::V6(Ipv6Addr::from(next))),
+			None => None,
+		};
+
+		match (cur, next) {
+			(Some(cur), Some(next)) => {
+				if self.net.contains(&cur) {
+					self.cur = Some(next);
+					Some(cur)
+				} else {
+					None
+				}
+			}
+			(Some(cur), None) => {
+				self.cur = next;
+				Some(cur)
+			}
+			(None, _) => None,
+		}
+	}
 }
 
 pub struct Netv4AddrAddressIterator {
