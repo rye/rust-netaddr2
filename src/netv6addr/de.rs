64050350036fdeea6e7d9bcae5a2bb30d11a30c0
Kristofer Rye <kristofer.rye@gmail.com>
2020-01-12 16:31:04 -0600

    Netv6Addr: Add assert_de_tokens_error test for errors
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/netv6addr/de.rs
+++ src/netv6addr/de.rs
@@ -30,7 +30,15 @@ impl<'de> Deserialize<'de> for Netv6Addr {
 #[cfg(test)]
 mod tests {
 	use super::Netv6Addr;
-	use serde_test::{assert_de_tokens, Token};
+	use serde_test::{assert_de_tokens, assert_de_tokens_error, Token};
+
+	#[test]
+	fn malformed_produces_correct_error() {
+		assert_de_tokens_error::<Netv6Addr>(
+			&[Token::Str("asdf")],
+			"invalid value: string \"asdf\", expected a valid cidr/extended network address",
+		)
+	}
 
 	#[test]
 	fn test_de_cidr_localhost() {
