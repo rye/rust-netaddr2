713efdd353be4606e4541ec780b8406aa9d7d3ac
Kristofer Rye <kristofer.rye@gmail.com>
2020-02-15 08:08:49 -0600

    Add some tests for Netv4Addr containment (non-CIDR)
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/netv4addr/contains.rs
+++ src/netv4addr/contains.rs
@@ -34,21 +34,37 @@ impl Contains<Netv4Addr> for Netv4Addr {
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	use std::net::Ipv4Addr;
+	mod cidr {
+		use crate::{Contains, Netv4Addr};
+		use std::net::Ipv4Addr;
 
-	#[test]
-	fn ip() {
-		let net: Netv4Addr = "127.0.0.1/8".parse().unwrap();
-		assert!(net.contains(&Ipv4Addr::new(127, 0, 0, 1)));
-		assert!(net.contains(&Ipv4Addr::new(127, 127, 255, 1)));
-		assert!(!net.contains(&Ipv4Addr::new(64, 0, 0, 0)));
+		#[test]
+		fn ip() {
+			let net: Netv4Addr = "127.0.0.1/8".parse().unwrap();
+			assert!(net.contains(&Ipv4Addr::new(127, 0, 0, 1)));
+			assert!(net.contains(&Ipv4Addr::new(127, 127, 255, 1)));
+			assert!(!net.contains(&Ipv4Addr::new(64, 0, 0, 0)));
+		}
+
+		#[test]
+		fn net() {
+			let net: Netv4Addr = "127.0.0.1/8".parse().unwrap();
+			let net_inner: Netv4Addr = "127.128.0.1/24".parse().unwrap();
+			assert!(net.contains(&net_inner));
+		}
 	}
 
-	#[test]
-	fn net() {
-		let net: Netv4Addr = "127.0.0.1/8".parse().unwrap();
-		let net_inner: Netv4Addr = "127.128.0.1/24".parse().unwrap();
-		assert!(net.contains(&net_inner));
+	mod non_cidr {
+		use crate::{Contains, Netv4Addr};
+		use std::net::Ipv4Addr;
+
+		#[test]
+		fn ip() {
+			let net: Netv4Addr = "127.255.255.0/255.127.127.0".parse().unwrap();
+
+			assert!(net.contains(&Ipv4Addr::new(127, 255, 127, 0)));
+			assert!(net.contains(&Ipv4Addr::new(127, 255, 127, 255)));
+			assert!(!net.contains(&Ipv4Addr::new(255, 127, 127, 0)));
+		}
 	}
 }
