05ae916bc97c4a0ce098ad89bc8ec6afac125b9e
Kristofer Rye <kristofer.rye@gmail.com>
2021-07-08 06:55:18 -0500

    iter::address: Expand documentation for AddressIterator
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/iter/address.rs
+++ src/iter/address.rs
@@ -1,6 +1,26 @@
 use crate::{Contains, Offset};
 
 /// An iterator over a network's _contained addresses_.
+///
+/// Starts from the "network address", the first address contained in the range, and iterates to the
+/// end of the network, the last address contained in the specified network.
+///
+/// # Examples
+///
+/// ```rust
+/// # use netaddr2::{Netv4Addr,AddressIterator};
+/// # use std::net::Ipv4Addr;
+/// let network: Netv4Addr = "10.0.0.1/30".parse().unwrap();
+///
+/// let mut iter = network.addresses();
+///
+/// // A /30 network only contains four addresses.
+/// assert_eq!(iter.next(), Some("10.0.0.0".parse().unwrap()));
+/// assert_eq!(iter.next(), Some("10.0.0.1".parse().unwrap()));
+/// assert_eq!(iter.next(), Some("10.0.0.2".parse().unwrap()));
+/// assert_eq!(iter.next(), Some("10.0.0.3".parse().unwrap()));
+/// assert_eq!(iter.next(), None);
+/// ```
 pub struct AddressIterator<Network, Address>
 where
 	Network: Contains<Address> + From<Address>,
