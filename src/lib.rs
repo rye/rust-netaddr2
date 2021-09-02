37e2a94f85d7db6038e3e7fe7cb8937ce04cf188
Kristofer Rye <kristofer.rye@gmail.com>
2021-09-02 10:00:32 -0500

    crate: Spruce up crate-level documentation a bit more
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-//! A crate for parsing, representing, and manipulating network addresses.
+//! A crate for storing, manipulating, and reasoning about network addresses.
 //!
 //! `netaddr2` arose from a need to mask and subnet IP space logically similar
-//! to the way that routers and network interface cards (NICs) do.  This enables
-//! the user to ask questions like:
+//! to the way that networking gear does, for example in simulating Access
+//! Control List (ACL) applicability.
 //!
-//! - Does this `IpAddr` belong to this `NetAddr`? (That is, does this specific
-//!   address happen to lie within the network/netmask given by this `NetAddr`?)
+//! This crate enables programmers to ask questions like:
+//!
+//! - Does this IP address belong to this network?
 //!   e.g.,
 //!   ```rust
 //!   # use std::net::IpAddr;
@@ -28,6 +29,9 @@
 //!   let all: netaddr2::NetAddr = "0.0.0.0/0".parse().unwrap();
 //!   assert!(!net.contains(&all));
 //!   ```
+//!
+//! You can also build IPv4- and IPv6-specific variants of the [NetAddr] struct;
+//! these are named [Netv4Addr] and [Netv6Addr] respectively.
 //!
 //! The API is strikingly similar to that of the `std::net::Ip.*Addr` structs,
 //! and users who have used that set of structs will hopefully find this API
